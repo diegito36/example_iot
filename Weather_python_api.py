2e46eb96c754031035cfc8c6663c06149243b4fe
diegito36 <diegito36@hotmail.com>
2016-04-16 00:43:58 +0000

    [A

---
+++ Weather_python_api.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import pywapi
+
+weather_com_result = pywapi.get_weather_from_weather_com('COXX0004')
+
+print "Weather.com says: It is " + 
+weather_com_result['current_conditions']['text'].lower() + " and " + 
+weather_com_result['current_conditions']['temper
+ature'] + "C now in Bogota."
